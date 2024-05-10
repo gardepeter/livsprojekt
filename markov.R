0fcd0efce4a6859e15d10e14c15f60b0cbc210c6
Garde <peter01garde@gmail.com>
2024-05-10 11:26:14 +0200

    data to anna

--- markov.R
+++ markov.R
@@ -2,7 +2,11 @@ options(scipen = 99)
 library(Rcpp)
 library(RcppArmadillo)
 
-Rcpp::sourceCpp("logic/cpp/RungaKutta/RK4.cpp")
+Rcpp::sourceCpp("logic/cpp/RungaKutta/RK4Markov.cpp")
 start = 0
-termination = 47
-RK4(start, termination, (termination - start) * 12)
+termination = 50
+probabilities = RK4(start, termination, (termination - start) * 12)
+colnames(probabilities) <- c("p_00", "p_01", "p_02",
+                             "p_10", "p_11", "p_12",
+                             "p_20", "p_21", "p_22")
+write.csv(probabilities, "probabilities.csv")
