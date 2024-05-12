8010e0d032e4c1ca5c8917e2da8b142d4356b283
Garde <peter01garde@gmail.com>
2024-05-12 17:54:44 +0200

    Major part of semiMarkov kernel impl.

--- semiMarkov.R
+++ semiMarkov.R
@@ -1,12 +1,12 @@
 #options(scipen = 99)
-library(Rcpp)
-library(RcppArmadillo)
-
 Rcpp::sourceCpp("logic/RK1SemiMarkov.cpp")
+
 startTime = 5.0
-stepAmount = 52
-endTime = 45.0
+endTime = 10.0
+stepAmount = 12 * (endTime - startTime)
 startDuration = 26 / stepAmount
 
 RK1(startTime, startDuration, endTime, stepAmount)
+
+temp = readr::read_csv("p00.csv", col_names = F)
 
