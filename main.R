60349211f67d365afb637979657c2b853ff1b0ff
nbv395 <nbv395@alumni.ku.dk>
2024-05-13 10:13:31 +0200

    Plot of probabilities done

--- main.R
+++ main.R
@@ -17,29 +17,43 @@ probabilities$age_in_month<-c(1:600)
 probabilities_0<-probabilities[,c(1:3,10)]
 probabilities_1<-probabilities[,c(4:6,10)]
 
-probabilitis_0_long<-pivot_longer(probabilities,c(p_00,p_01,p_02),
-                  names_to="transitions",
-                  values_to="probabilities")
+probabilities_0_long <- pivot_longer(probabilities_0, c(p_00, p_01, p_02),
+                                     names_to = "transitions",
+                                     values_to = "probabilities")
 
-probabilitis_1_long<-pivot_longer(probabilities,c(p_10,p_11,p_12),
-                                  names_to="transitions",
-                                  values_to="probabilities")
+probabilities_1_long <- pivot_longer(probabilities_1, c(p_10, p_11, p_12),
+                                     names_to = "transitions",
+                                     values_to = "probabilities")
 
-grid.arrange(
-  ggplot(probabilitis_0_long, aes(x = age_in_month, y = probabilities, color = transitions)) +
-  geom_line()+
-    labs(title="From active state")+
-    ylab(bquote(p[ij]))+
-    theme(legend.position="bottom",
-          legend.title=element_blank()),
-  ggplot(probabilitis_1_long, aes(x = age_in_month, y = probabilities, color = transitions)) +
-    geom_line()+
-    labs(title="From disabled state")+
-    theme(axis.title.y=element_blank(),
-          legend.position="bottom",
-          legend.title=element_blank())+
-    scale_fill_discrete(name = "transitions", labels = c("A", "B", "C")),
-  nrow=1
-)
+legend_labels <- c(expression(paste("p"["00"])), 
+                   expression(paste("p"["01"])),
+                   expression(paste("p"["02"])),
+                   expression(paste("p"[10])),
+                   expression(paste("p"[11])),
+                   expression(paste("p"[12])))
+
+plot_0 <- ggplot(probabilities_0_long, aes(x = age_in_month, y = probabilities, color = transitions)) +
+  geom_line() +
+  labs(title = "From active state") +
+  ylab("Probabilities") +
+  scale_color_discrete(labels = legend_labels[1:3]) +  
+  theme(legend.position = "bottom",
+        legend.title = element_blank())
+
+plot_1 <- ggplot(probabilities_1_long, aes(x = age_in_month, y = probabilities, color = transitions)) +
+  geom_line() +
+  labs(title = "From disabled state") +
+  theme(axis.title.y = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_blank()) +
+  scale_color_discrete(labels = legend_labels[4:6])  
+
+# Combine plots using grid.arrange
+grid.arrange(plot_0, plot_1, nrow = 1)
+
+#Cashflow
+
+
+
 
 
