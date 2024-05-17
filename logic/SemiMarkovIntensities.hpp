0879d4353a64608d90f6fa34118fc46b90bb8edd
Garde <peter01garde@gmail.com>
2024-05-17 11:53:34 +0200

    fix cpp

--- logic/SemiMarkovIntensities.hpp
+++ logic/SemiMarkovIntensities.hpp
@@ -11,13 +11,13 @@ double mu01(double x) {
     return 0.0009687435;
   }
   
-  return exp(72.53851 - 10.66927* x + 0.53371 * pow(x, 2) − 0.012798 * pow(x, 3)
-             0.00014922 * pow(x, 4) - 0.00000068007 * pow(x, 6));
+  return exp(72.53851 - 10.66927* x + 0.53371 * pow(x, 2) - 0.012798 * pow(x, 3)
+             + 0.00014922 * pow(x, 4) - 0.00000068007 * pow(x, 6));
 }
 
 double mu10(double x, double d) {
   if(d <= 0.2291667){
-    return exp( -0.9148875 - 0.0309126 * x + 4.8715347 * d )
+    return exp( -0.9148875 - 0.0309126 * x + 4.8715347 * d );
   }
   if(d > 0.2291667 && d <= 2.){
     return exp( 0.3766531 - 0.0309126 * x - 0.7642786 * d );
@@ -31,10 +31,10 @@ double mu10(double x, double d) {
 }
 
 double mu12(double x, double d) {
-  if(u <= 5){
+  if(d <= 5){
     return exp( -6.1057464 + 0.0635736 * x - 0.2891195 * d );
   }
-  if(u > 5){
+  if(d > 5){
     return exp( - 11.9169277 + 0.1356766 * x );
   }
 }
