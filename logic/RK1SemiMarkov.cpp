3085785447ff1187f753db202d392ba26398307c
Garde <peter01garde@gmail.com>
2024-05-20 13:05:19 +0200

    Age impl. in int. First "real" validation

--- logic/RK1SemiMarkov.cpp
+++ logic/RK1SemiMarkov.cpp
@@ -2,61 +2,61 @@
 #include "SemiMarkovIntensities.hpp"
 const double EPSILON = 0.00001;
 
-double intensityOutOfState(int j, double s, double d){
+double intensityOutOfState(int j, double s, double d, double age){
   double sum = 0;
   
   for(int k = 0; k < states; k++){
     if(k == j){
       continue;
     }
-    sum += mu(j, k, s, d);
+    sum += mu(j, k, s, d, age);
   }
   
   return sum;
 }
 
 // d=x*h
-double leftIntegral(arma::cube& probabilities, double t0, int i, int j, double s, int d_step, double stepLength, int iteration){ 
+double leftIntegral(arma::cube& probabilities, double t0, int i, int j, double s, int d_step, double stepLength, int iteration, double age){ 
   double trapezoidalSum=0;
-  for(int n=1; n < d_step; n++){ // this is for the y-coordinate of the probability matrix
+  for(int n=1; n <= d_step; n++){ // this is for the y-coordinate of the probability matrix
     // s=t0+xh 
     trapezoidalSum += 0.5*(probabilities(n, iteration, states * i + j) - probabilities(n-1, iteration, states * i + j))
-    *(intensityOutOfState(j,s,n*stepLength)+intensityOutOfState(j,s,stepLength*(n-1)));
+    *(intensityOutOfState(j,s,n*stepLength, age)+intensityOutOfState(j,s,stepLength*(n-1), age));
   }
   return trapezoidalSum;
 }
 
 //TODO // the variable d is not in use and could be removed 
 // the function should work as long as u=x*h and i have not checked if it works if not 
-double trapezoidalRightEstimation(arma::cube& probabilities, double t0, double u, int i, int j, int l, double s, double stepLength, int iteration){ 
+double trapezoidalRightEstimation(arma::cube& probabilities, double t0, double u, int i, int j, int l, double s, double stepLength, int iteration, double age){ 
   double trapezoidalSum=0;
-  for(int n=1; n< round((u+s-t0)/stepLength); n++){ // this is for the y-coordinate of the probability matrix
+  for(int n=1; n <= round((u+s-t0)/stepLength); n++){ // this is for the y-coordinate of the probability matrix
     // s=t0+xh 
     trapezoidalSum += 0.5*(probabilities(n, iteration, states * i + l) - probabilities(n-1, iteration, states * i + l))
-    *(mu(l,j,s,n*stepLength)+mu(l,j,s,stepLength*(n-1)));
+    *(mu(l,j,s,n*stepLength, age)+mu(l,j,s,stepLength*(n-1), age));
   }
   return trapezoidalSum;
 }
 
-double rightSumOfIntegrals(arma::cube& probabilities, double t0, double u, int i, int j, double s, double stepLength, int iteration){
+double rightSumOfIntegrals(arma::cube& probabilities, double t0, double u, int i, int j, double s, double stepLength, int iteration, double age){
   double rightSumResult = 0;
   for(int l = 0; l < states; l++){
     if(l == j){
       continue;
     }
-    rightSumResult += trapezoidalRightEstimation(probabilities, t0, u, i, j, l, s, stepLength, iteration);
+    rightSumResult += trapezoidalRightEstimation(probabilities, t0, u, i, j, l, s, stepLength, iteration, age);
   }
   return rightSumResult;
 }
 
-void RK1Step(arma::cube& probabilities, double startTime, double startDuration, int iteration, double stepLength, int states){
+void RK1Step(arma::cube& probabilities, double startTime, double startDuration, int iteration, double stepLength, int states, double age){
   for(int i = 0; i < states; i++){
     for(int j = 0; j < states; j++){
-      double rightSum = rightSumOfIntegrals(probabilities, startTime, startDuration, i, j, startTime + stepLength * (iteration  - 1), stepLength, iteration - 1);
+      double rightSum = rightSumOfIntegrals(probabilities, startTime, startDuration, i, j, startTime + stepLength * (iteration  - 1), stepLength, iteration - 1, age);
       
-      for(int d_step = 1; d_step < (int)floor(startDuration/stepLength) + iteration; d_step++){
+      for(int d_step = 1; d_step <= (int)floor(startDuration/stepLength) + iteration; d_step++){
         probabilities(d_step, iteration, states * i + j) = probabilities(d_step - 1, iteration - 1, states * i + j)
-          + stepLength * ( - leftIntegral(probabilities, startDuration, i, j, startTime + stepLength * iteration, d_step-1, stepLength, iteration - 1) + rightSum); //TODO investegate if d_step - 1 or not
+          + stepLength * ( - leftIntegral(probabilities, startDuration, i, j, startTime + stepLength * iteration, d_step-1, stepLength, iteration - 1, age) + rightSum); //TODO investegate if d_step - 1 or not
       }
     }
   }
@@ -72,13 +72,6 @@ bool isNotMultipla(double x, double y){
   return abs(x*y - trunc(x*y)) >= EPSILON;
 }
 
-
-//For debugging uses
-// void saveCube(arma::cube& probabilities, int states){
-//   for(int j = 0; j < states; j++){
-//       probabilities.slice(j).save("p0" + std::to_string(j) + ".csv", arma::csv_ascii);
-//   }
-// }
 void saveCube(arma::cube& probabilities, int states){
   for(int i = 0; i < states - 1; i++){
     for(int j = 0; j < states; j++){
@@ -87,13 +80,7 @@ void saveCube(arma::cube& probabilities, int states){
   }
 }
 
-// [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::export]]
-int RK1(double startTime, double startDuration, double endTime, int stepAmountPerTimeUnit) {
-  if( endTime <= startTime ||  stepAmountPerTimeUnit <= 1 || isNotMultipla(startDuration, (double)stepAmountPerTimeUnit)){
-    return -1;
-  }
-
+arma::cube RK1_Cpp(double startTime, double startDuration, double endTime, int stepAmountPerTimeUnit, double age) {
   double stepLength = 1. / (double)stepAmountPerTimeUnit;
   
   int stepsFromZeroToStartDuration = (int)round(startDuration * stepAmountPerTimeUnit);
@@ -104,10 +91,48 @@ int RK1(double startTime, double startDuration, double endTime, int stepAmountPe
   boundaryCondition(probabilities, stepsFromZeroToStartDuration );
   
   for(int iteration = 1; iteration < stepAmountPerTimeUnit * (endTime - startTime); iteration++){
-    RK1Step(probabilities, startTime, startDuration, iteration, stepLength, states);
+    RK1Step(probabilities, startTime, startDuration, iteration, stepLength, states, age);
   }
+
+  return probabilities;
+}
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+int RK1(double startTime, double startDuration, double endTime, int stepAmountPerTimeUnit, double age) {
+  if( endTime <= startTime ||  stepAmountPerTimeUnit <= 1 || isNotMultipla(startDuration, (double)stepAmountPerTimeUnit)){
+    return -1;
+  }
+  
+  arma::cube probabilities = RK1_Cpp(startTime, startDuration, endTime, stepAmountPerTimeUnit, age);
   
   saveCube(probabilities, states);
   return 0;
 }
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+arma::vec unitCashflowDisabilityWithKarens(double startTime, double startDuration, double endTime, int stepAmountPerTimeUnit, double gracePeriod, double age, int i, int j) {
+  arma::cube probabilities = RK1_Cpp(startTime, startDuration, endTime, stepAmountPerTimeUnit, age);
+  
+  arma::mat P11 =  probabilities.slice(states * i + j);
+  
+  int cashflowLength = stepAmountPerTimeUnit * (endTime - startTime);
+  int stepsFromZeroToStartDuration = (int)round(startDuration * stepAmountPerTimeUnit);
+  
+  arma::vec cashflow(cashflowLength);
+  for(int i = 0; i < cashflowLength; i++ ){
+    if(stepsFromZeroToStartDuration + i < gracePeriod + 1){
+      continue;
+    }
+    cashflow(i) = P11(stepsFromZeroToStartDuration + i, i) - P11(gracePeriod + 1, i); // Strict ineq. as gracePeriod <= 3 month
+  }
+  
+  return cashflow;
+}
+
+
+
+
+
 
---
+++ semiMarkovValidation.R
@@ -0,0 +1,76 @@
+#options(scipen = 99)
+library(tidyverse)
+
+Rcpp::sourceCpp("logic/RK1SemiMarkov.cpp")
+
+########## UTILITIES ##################
+f = function(matrix, index, amount){
+  vector = c()
+  for(i in 1:amount){
+    vector[i] = matrix[index + i, i]
+  }
+  return(unlist(vector))
+}
+
+############## SUM OF DIAGANOL VALIDATION ##############
+age = 20
+startTime = 0.0
+endTime = 40.0
+stepAmountPerTimeUnit = 12
+startIncrement = 0 * stepAmountPerTimeUnit
+startDuration = startIncrement / stepAmountPerTimeUnit
+
+RK1(startTime, startDuration, endTime, stepAmountPerTimeUnit, age)
+
+p00 = readr::read_csv("p00.csv", col_names = F)
+p01 = readr::read_csv("p01.csv", col_names = F)
+p02 = readr::read_csv("p02.csv", col_names = F)
+
+diagonal_vector_00 = f(p00, startIncrement, stepAmountPerTimeUnit * (endTime - startTime))
+diagonal_vector_01 = f(p01, startIncrement, stepAmountPerTimeUnit * (endTime - startTime))
+diagonal_vector_02 = f(p02, startIncrement, stepAmountPerTimeUnit * (endTime - startTime))
+
+p10 = readr::read_csv("p10.csv", col_names = F)
+p11 = readr::read_csv("p11.csv", col_names = F)
+p12 = readr::read_csv("p12.csv", col_names = F)
+
+diagonal_vector_10 = f(p10, startIncrement, stepAmountPerTimeUnit * (endTime - startTime))
+diagonal_vector_11 = f(p11, startIncrement, stepAmountPerTimeUnit * (endTime - startTime))
+diagonal_vector_12 = f(p12, startIncrement, stepAmountPerTimeUnit * (endTime - startTime))
+
+#SHOULD be zero
+tolerance = 0.01
+sum(abs(diagonal_vector_00 + diagonal_vector_01 + diagonal_vector_01 - 1) > tolerance)
+sum(abs(diagonal_vector_10 + diagonal_vector_11 + diagonal_vector_12 - 1) > tolerance)
+
+
+################ PLOTS FOR VALIDATION ####################
+age = 40
+startTime = 0.0
+endTime = 25.0
+stepAmountPerTimeUnit = 12
+startIncrement = c(0, 1) * stepAmountPerTimeUnit
+karensPeriod = 3
+startDuration = startIncrement / stepAmountPerTimeUnit
+
+cashflow_0 = unitCashflowDisabilityWithKarens(startTime, startDuration[1], endTime, stepAmountPerTimeUnit, karensPeriod, age, 1, 1)
+cashflow_1 = unitCashflowDisabilityWithKarens(startTime, startDuration[2], endTime, stepAmountPerTimeUnit, karensPeriod, age, 1, 1)
+
+plot_pre = cbind(tibble(age = seq(age + startTime, age + endTime, length.out =  stepAmountPerTimeUnit * (endTime - startTime) )),
+             cashflow_0,
+             cashflow_1) 
+#write.csv(plot_pre, "unitCashflows.csv", row.names = F)
+
+plot = plot_pre %>%
+  pivot_longer(!age, values_to = "value", names_to = "cashflow")
+
+ggplot(plot, aes(age, value))+
+  geom_line() + 
+  scale_y_continuous(breaks = seq(0, 1.2, length.out = 7), limits = c(0, 1.2)) +
+  facet_wrap(~cashflow)
+#TODO remove y label and change title names to the same as article p. 26. Ahmad, Bladt, and Furrer, 2024
+
+
+
+
+
