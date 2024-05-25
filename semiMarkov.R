55ab8d2325e10ac40fa9dacb78d70f190b37807e
Garde <peter01garde@gmail.com>
2024-05-25 20:29:33 +0200

    vhanges

--- semiMarkov.R
+++ semiMarkov.R
@@ -1,7 +1,7 @@
 #****************************************************
 #*************** SEE semiMarkovValidation.R **********
 #*#***************************************************
-# library(tidyverse)
+library(tidyverse)
 options(scipen =99)
 Rcpp::sourceCpp("logic/RK1SemiMarkov.cpp")
 
@@ -13,12 +13,14 @@ startIncrement = 0
 karensPeriod = 1/4
 startDuration = startIncrement / stepAmountPerTimeUnit
 
+
+cashflow =read_csv("./data/unitDisabilityBenefitCashflow_3MonthGracePeriod_20YearsOldActive2024.csv")
 # cashflow = unitCashflowDisabilityWithKarens(startTime, startDuration, endTime, stepAmountPerTimeUnit, karensPeriod, age, 0, 1)
-# rate_cont = approxfun(unlist(spot_rate[,1]), unlist(spot_rate[,2]))
-# bond_price = sapply(seq(0, 50), function(x) exp(-integrate(rate_cont, 0, x)$val))
-# bond_price_cont = approxfun(seq(0, 50), bond_price)
-# integrand = approxfun(unlist(cashflow[,1]),unlist(cashflow[,2]) * bond_price_cont(unlist(cashflow[,1])))
-# integrate(integrand, 0, 49.916)$val
+rate_cont = approxfun(unlist(spot_rate[,1]), unlist(spot_rate[,2]))
+bond_price = sapply(seq(0, 50), function(x) exp(-integrate(rate_cont, 0, x)$val))
+bond_price_cont = approxfun(seq(0, 50), bond_price)
+integrand = approxfun(unlist(cashflow[,1]),unlist(cashflow[,2]) * bond_price_cont(unlist(cashflow[,1])))
+integrate(integrand, 0, 49.916)$val
 
 system.time({
   # cashflow_slow = unitCashflowDisabilityWithKarens(startTime, startDuration, endTime, stepAmountPerTimeUnit, age, karensPeriod, 0, 1)
