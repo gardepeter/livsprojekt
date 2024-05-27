6071bc89def4434cb1f5331d9fe13f51ad0bc095
pbw633 <jenshertzfischer@gmail.com>
2024-05-27 15:12:13 +0200

    Integrand function and diagonal matrix function made

--- logic/aggregateMarkov.cpp
+++ logic/aggregateMarkov.cpp
@@ -1,6 +1,36 @@
 #include "RcppArmadillo.h"
 
 // STEP 1
+const int scenario=2;
+const int states=3;
+
+int di(int macrostate, int scenario){
+  result=1;
+  if(scenario==2){
+    if(macrostate==1){
+      result=2;
+    }
+  }else if(scenario==3){
+    if(macrostate==1){
+      result=3;
+    }
+  }else if(scenario==4){
+    if(macrostate==1){
+      result=5;
+    }
+  }else if(scenario==4){
+    if(macrostate==1){
+      result=7;
+    }
+  }
+  else if(scenario==4){
+    if(macrostate==1){
+      result=10;
+    }
+  }
+  return result;
+}
+
 arma::mat prodIntegralSolver(double s, double t, arma::mat& M){
   return arma::mat();
 }
@@ -11,6 +41,7 @@ arma::mat initialConditionalDistribution(){
 
 // STEP 2
 //TODO: can and should be parralized
+// should take some short of cube in as an input
 arma::cube allProbabilitySolver(double startTIme, double endTime){
   return arma::cube();
 }
@@ -20,8 +51,27 @@ arma::mat probabilitySolver(double startTIme, double endTime, arma::cube& allPro
 }
 // STEP 3
 //// i)
-arma::mat diagonolMatrix(double startTime, double endTime, arma::cube& microStateProbabilities){
-  return arma::mat();
+arma::mat diagonolMatrix(double startTime, 
+                         double endTime,
+                         int dbar,
+                         arma::cube& microStateProbabilities){
+  //creates a matrix that is big enough to have each probability matrix in its diagonal
+  arma::mat result=mat(dbar,dbar);
+  int state=0; // start with active 
+  for(int l=0; i<states; l++){ // for each state
+    int displacement=0;
+    //we look at the matrix P for each macro state where only for l=1 (sick) the dimension changes
+    for(int i=0; i<di(state,scenario); i++){
+      for(int j=0; j<di(state,scenario); j++ ){
+        // we copy matrix P_kk into the diagonal
+        result(i+displacement,j+displacement)=P(state,i,j); 
+      }
+    }
+    //update the displacement such that we can copy another matrix into the diagonal
+    displacement += di(state,scenario);
+  }
+  
+  return result;
 }
 
 //// ii)
@@ -33,7 +83,14 @@ arma::vec integrand(double t,
                     arma::mat& PBar,
                     arma::mat& R,
                     int dBar){
-  return arma::vec();
+  //created 1_dbar and fills it in
+  arma::mat oneColumndbar= mat(dbar,1);
+  for(int i=0; i<dbar; i++){
+    oneColumndbar(i,1)=1;
+  }
+  // the result where we have assumed that the matrices is already calculated for the specific t and v
+  arma::mat result=P*MTilde*R*oneColumndbar;
+  return result;
 }
 
 arma::vec rightTerm(double t, 
